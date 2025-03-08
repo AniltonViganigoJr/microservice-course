6d8cf01b749048abbadd848ba1ebc8e46e028545
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2025-03-07 23:40:05 -0300

    MS Cloud Gateway created

---
+++ mscloudgateway/src/test/java/io/github/cursoms/mscloudgateway/MscloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cursoms.mscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
